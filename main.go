c117f9af30f33c11bcc44147802818826686bb06
PSL2310 <philipp.samuel.ludin@gmail.com>
2026-05-13 16:54:19 +0200

    added validation handler and improved metrics response

--- main.go
+++ main.go
@@ -5,7 +5,11 @@ import (
 	"log"
 	"sync/atomic"
 	"fmt"
+	"encoding/json"
+	"strings"
 )
+
+
 
 func main() {
 	apiCfg := apiConfig{}
@@ -15,9 +19,10 @@ func main() {
     	Handler: serverMux,
 	}
 	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	serverMux.HandleFunc("GET /healthz", HandlerFunction)
-	serverMux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	serverMux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	serverMux.HandleFunc("GET /api/healthz", HandlerFunction)
+	serverMux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidation)
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
@@ -44,7 +49,14 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+    w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
@@ -52,3 +64,70 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 }
 
 
+func (cfg *apiConfig) handlerValidation(w http.ResponseWriter, r *http.Request) {
+    type parameters struct {
+        Body string `json:"body"`
+    }
+
+    decoder := json.NewDecoder(r.Body)
+    params := parameters{}
+    err := decoder.Decode(&params)
+    if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+    }
+
+    type validReturn struct {
+        Valid bool `json:"valid"`
+    }
+
+	type errorReturn struct {
+		Error string `json:"error"`
+	}
+
+	type cleanedReturn struct {
+		Body string `json:"cleaned_body"`
+	}
+	
+	badWords := map[string]struct{}{
+    "kerfuffle": {},
+    "sharbert":  {},
+    "fornax":    {},
+}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	body := strings.Split(params.Body, " ")
+	for i, word := range body {
+    if _, ok := badWords[strings.ToLower(word)]; ok {
+        body[i] = "****"
+    }
+}
+
+	params.Body = strings.Join(body, " ")
+	respondWithJSON(w, 200, cleanedReturn{Body: params.Body})
+    
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+    data, err := json.Marshal(payload)
+    if err != nil {
+        log.Printf("Error marshalling JSON: %s", err)
+        w.WriteHeader(500)
+        return
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(code)
+    w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+    type errorReturn struct {
+        Error string `json:"error"`
+    }
+    respondWithJSON(w, code, errorReturn{Error: msg})
+}
