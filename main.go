ab6bc1cf305544d5a12271b622adf56d50d7fd7b
PSL2310 <philipp.samuel.ludin@gmail.com>
2026-05-15 09:56:23 +0200

    add database functionality with user creation and deletion, include .env support

--- main.go
+++ main.go
@@ -1,32 +1,46 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"sync/atomic"
-	"fmt"
+	"database/sql"
 	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"strings"
+	"sync/atomic"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
+
+	"httpserver/internal/database"
 )
 
-
-
 func main() {
-	apiCfg := apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	platForm := os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
+	dbQueries := database.New(db)
+	apiCfg := apiConfig{database: dbQueries, platform: platForm}
 	serverMux := http.NewServeMux()
 	server := &http.Server{
-    	Addr:    ":8080",
-    	Handler: serverMux,
+		Addr:    ":8080",
+		Handler: serverMux,
 	}
+
 	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	serverMux.HandleFunc("POST /api/users", apiCfg.handlerUsers)
 	serverMux.HandleFunc("GET /api/healthz", HandlerFunction)
 	serverMux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidation)
 	serverMux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	serverMux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
-	}
+	server.ListenAndServe()
 }
 
 func HandlerFunction(w http.ResponseWriter, r *http.Request) {
@@ -35,22 +49,22 @@ func HandlerFunction(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
 }
 
-
-
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database *database.Queries
+	platform string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        cfg.fileserverHits.Add(1)
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintf(`<html>
   <body>
     <h1>Welcome, Chirpy Admin</h1>
@@ -60,27 +74,68 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-    cfg.fileserverHits.Store(0)
+	if cfg.platform != "dev" {
+		w.WriteHeader(403)
+		return
+	}
+	err := cfg.database.DeleteUsers(r.Context())
+	if err != nil {
+		log.Printf("Error deleting database: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(200)
 }
 
+func (cfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
 
-func (cfg *apiConfig) handlerValidation(w http.ResponseWriter, r *http.Request) {
-    type parameters struct {
-        Body string `json:"body"`
-    }
-
-    decoder := json.NewDecoder(r.Body)
-    params := parameters{}
-    err := decoder.Decode(&params)
-    if err != nil {
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
 		log.Printf("Error decoding parameters: %s", err)
 		w.WriteHeader(500)
 		return
-    }
+	}
 
-    type validReturn struct {
-        Valid bool `json:"valid"`
-    }
+	usr, err := cfg.database.CreateUser(r.Context(), params.Email)
+	if err != nil {
+		fmt.Printf("%v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	type emailDataReturn struct {
+		Id uuid.UUID `json:"id"`
+		CreatedDate time.Time `json:"created_at"`
+		UpdateDate time.Time `json:"updated_at"`
+		Email string `json:"email"`
+	}
+
+	respondWithJSON(w, 201, emailDataReturn{Id: usr.ID, CreatedDate: usr.CreatedAt, UpdateDate: usr.UpdatedAt, Email: usr.Email})
+
+}
+
+func (cfg *apiConfig) handlerValidation(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	type validReturn struct {
+		Valid bool `json:"valid"`
+	}
 
 	type errorReturn struct {
 		Error string `json:"error"`
@@ -89,12 +144,12 @@ func (cfg *apiConfig) handlerValidation(w http.ResponseWriter, r *http.Request) 
 	type cleanedReturn struct {
 		Body string `json:"cleaned_body"`
 	}
-	
+
 	badWords := map[string]struct{}{
-    "kerfuffle": {},
-    "sharbert":  {},
-    "fornax":    {},
-}
+		"kerfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
 
 	if len(params.Body) > 140 {
 		respondWithError(w, 400, "Chirp is too long")
@@ -103,31 +158,31 @@ func (cfg *apiConfig) handlerValidation(w http.ResponseWriter, r *http.Request) 
 
 	body := strings.Split(params.Body, " ")
 	for i, word := range body {
-    if _, ok := badWords[strings.ToLower(word)]; ok {
-        body[i] = "****"
-    }
-}
+		if _, ok := badWords[strings.ToLower(word)]; ok {
+			body[i] = "****"
+		}
+	}
 
 	params.Body = strings.Join(body, " ")
 	respondWithJSON(w, 200, cleanedReturn{Body: params.Body})
-    
+
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload any) {
-    data, err := json.Marshal(payload)
-    if err != nil {
-        log.Printf("Error marshalling JSON: %s", err)
-        w.WriteHeader(500)
-        return
-    }
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(code)
-    w.Write(data)
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
-    type errorReturn struct {
-        Error string `json:"error"`
-    }
-    respondWithJSON(w, code, errorReturn{Error: msg})
+	type errorReturn struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, errorReturn{Error: msg})
 }
